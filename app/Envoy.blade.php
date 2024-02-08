db5cdc893a73d20d8c21040411d880369dd377ca
lorenzo-bravard <lorenzo@LAPTOP-59T1QMK9>
2024-02-08 16:40:18 +0100

    fix

--- app/Envoy.blade.php
+++ app/Envoy.blade.php
@@ -1,24 +1,15 @@
-@servers(['web' => 'lorenzo-bravard@13.39.150.46'])
-
-@story('deploy')
-    update-depandencies
-    update-db
-    optimize
-@endstory
-
-@task('update-depandencies', ['on' => 'web'])
+@servers(['production' => ['lorenzo-bravard@13.39.150.46']])
+ 
+@task('deploy', ['on' => 'production'])
     cd lorenzo-bravard.dhonnabhain.me
+    git checkout production
     cd app
     composer update
     npm install
     npm run build
-@endtask
-
-@task('update-db', ['on' => 'web'])
-    php artisan migrate 
-@endtask
-
-@task('optimize', ['on' => 'web'])
+    php artisan migrate --force
     composer install --optimize-autoloader --no-dev
+    php artisan optimize:clear
     php artisan optimize
+    php artisan key:generate
 @endtask
